836de8c3090adf95d7fa441a31b25f9df5f9de0c
Allyssa Batten <abatten@truefit.io>
2019-08-23 13:15:25 -0400

    Resetting the Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,10 +13,13 @@ class Quiz extends Component {
   }
 
   showNextQuestion() {
-    console.log("HERE");
     this.setState((state) => {
       return { quiz_position: state.quiz_position + 1 }
     });
+  }
+
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
   }
 
   render() {
@@ -25,7 +28,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ?
-          <QuizEnd /> :
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
           <QuizQuestion
             quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
             showNextQuestionHandler={this.showNextQuestion.bind(this)}
